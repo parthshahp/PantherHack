e66d56f823c4bec3c332e09e8f4a0ae6c5c19c70
parthshahp <parthshahp1@gmail.com>
2019-03-09 18:54:21 -0500

    commit2

--- app/src/main/java/com/example/pantherhack/Settings.java
+++ app/src/main/java/com/example/pantherhack/Settings.java
@@ -24,7 +24,6 @@ public class Settings extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
