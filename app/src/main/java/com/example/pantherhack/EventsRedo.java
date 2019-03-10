0a7edbc0140191b52d98ed439c7984b800e57b79
kbennett30 <41123869+kbennett30@users.noreply.github.com>
2019-03-10 00:44:00 -0500

    Event Page updates

---
+++ app/src/main/java/com/example/pantherhack/EventsRedo.java
@@ -0,0 +1,13 @@
+package com.example.pantherhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventsRedo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_redo);
+    }
+}
